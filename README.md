cc9ada5c8e306bf03bf59e1d8d87fe0da97eb0c5
curder <curder@foxmail.com>
2020-07-08 11:57:58 +0800

    post request demo

--- README.md
+++ README.md
@@ -6,3 +6,29 @@
 ```
 composer install -vvv
 ```
+
+use `try-catch` and if using PHP-7 can try to catch Throwable always with a slash as follow:
+
+```
+try {
+    $url = 'https://postman-echo.com/post';
+    $response = $client->post($url, [
+        'headers' => [
+            'api_key' => 'abc',
+        ],
+        'json' => [
+            'foo1' => 'bar1',
+            'foo2' => 'bar2',
+        ],
+    ]);
+    print_r($response->getStatusCode());
+    echo "\n";
+
+    print_r($response->getBody());
+
+}  catch (\Throwable $e) {
+    print_r($e->getResponse()->getStatusCode());
+    echo "\n";
+    print_r($e->getMessage());
+}
+```
--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+
 require('vendor/autoload.php');
 
+$client = new \GuzzleHttp\Client();
+
+try {
+    $url = 'https://postman-echo.com/post';
+    $response = $client->post($url, [
+        'headers' => [
+            'api_key' => 'abc',
+        ],
+        'json' => [
+            'foo1' => 'bar1',
+            'foo2' => 'bar2',
+        ],
+    ]);
+    print_r($response->getStatusCode());
+    echo "\n";
+
+    print_r($response->getBody());
+
+}  catch (\Throwable $e) {
+    print_r($e->getResponse()->getStatusCode());
+    echo "\n";
+    print_r($e->getMessage());
+}
